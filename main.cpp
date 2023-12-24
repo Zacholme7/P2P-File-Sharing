8748c1d5a571f8122bac9604ebab984253da13c7
Zachary Holme <zacholme@gmail.com>
2023-12-24 16:40:33 -0600

    implemented client server

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "http_tcpServer_linux.h"
+
+using namespace http;
+
+int main() {
+        TcpServer server = TcpServer("127.0.0.1", 8080);
+        server.startListen();
+        return 0;
+}
+
